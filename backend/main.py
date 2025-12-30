315129e7f5469643f87f79a9d3b958453bf1d2ee
nao-nba <ba.d.yoa.b.tw@gmail.com>
2025-12-30 11:14:49 +0900

    backendの変更がGitHub Actionsのトリガーとして反応するか確認

--- backend/main.py
+++ backend/main.py
@@ -58,6 +58,7 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World from FastAPI"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
