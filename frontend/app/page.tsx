2827233c0dddc986165a75b5295a47233b05eaa9
nao-nba <ba.d.yoa.b.tw@gmail.com>
2025-12-30 13:01:37 +0900

    frontend,backendの変更がGitHub Actionsのトリガーとして反応するか確認

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,6 +14,7 @@ export default fu(nction Home() {
       .catch((err) => console.error("Error fetching data:", err));
   }, []);
 
+  
   return (
     <main style={{ padding: '2rem', textAlign: 'center' }}>
       <h1>Next.js + FastAPI 疎通テスト</h1>
